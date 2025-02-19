478ac1f2ea07e845e24a961233beb8f9638738aa
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-02-19 17:51:27 +0100

    add: header and main part

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,29 @@
 function Header (){
     return(
         <header>
-            Header
+            <div className="container">
+                <figure>
+                    <img src="../src/assets/imgs/dc-logo.png" alt="logo"></img>
+                </figure>
+                <nav>
+                    <ul>
+
+                    <li><a href="#">CHARACTERS</a></li>
+                    <li className="active"><a className="active" href="#">COMICS</a></li>
+                    <li><a href="#">MOVIES</a></li>
+                    <li><a href="#">TV</a></li>
+                    <li><a href="#">GAMES</a></li>
+                    <li><a href="#">COLLECTIBLES</a></li>
+                    <li><a href="#">VIDEOS</a></li>
+                    <li><a href="#">FANS</a></li>
+                    <li><a href="#">NEWS</a></li>
+                    <li><a href="#">SHOP</a></li>
+
+                    </ul>
+                   
+                </nav>
+
+            </div>
         </header>
 
     )
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,61 @@
+import digital from "../assets/imgs/buy-comics-digital-comics.png"
+import merch from "../assets/imgs/buy-comics-merchandise.png"
+import shop from "../assets/imgs/buy-comics-shop-locator.png"
+import subscript from "../assets/imgs/buy-comics-subscriptions.png"
+import visa from "../assets/imgs/buy-dc-power-visa.svg"
+
+
 function Main (){
     return(
         <main>
-            Main
+            <div className="container_main_top">
+                <h3 className="title_main">
+                    --	&gt; Content goes here! &lt;--
+                </h3>
+            </div>
+
+            <div className="container_main_bottom">
+                <div className="container_icon_span">
+                    <figure>
+                        <img className="img_main" src={digital} alt="foto digital"></img> 
+                    </figure>
+                    <span className="span_main">DIGITAL COMICS</span>
+
+                </div>
+                <div className="container_icon_span">
+                    <figure>
+                        <img className="img_main" src={merch} alt="foto merch"></img> 
+                    </figure>
+                    <span className="span_main">DC MERCHANDISE</span>
+
+                </div>
+
+                <div className="container_icon_span">
+                    <figure>
+                        <img className="img_main" src={subscript} alt="foto digital"></img> 
+                    </figure>
+                    <span className="span_main">SUBSCRIPTION</span>
+
+                </div>
+
+                <div className="container_icon_span">
+                    <figure>
+                        <img className="img_main" src={shop} alt="foto digital"></img> 
+                    </figure>
+                    <span className="span_main">COMIC SHOP LOCATOR</span>
+
+                </div>
+
+                <div className="container_icon_span">
+                    <figure>
+                        <img className="img_main" src={visa} alt="foto digital"></img> 
+                    </figure>
+                    <span className="span_main"> DC POWER VISA </span>
+
+                </div>
+
+
+            </div>
         </main>
 
     )
